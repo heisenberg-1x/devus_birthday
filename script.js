e73aed78753199688b9750408a439acfb6f2f64b
heisenberg-1x <118151903+heisenberg-1x@users.noreply.github.com>
2024-08-19 16:17:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('gift-icon').addEventListener('click', function() {
+    // Replace the following URL with your actual Google Drive link
+    window.location.href = 'https://drive.google.com/file/d/1xOHI49CaqY2JuUuBUdXk1GiIv0gR5iWZ/view?usp=drive_link';
+});
